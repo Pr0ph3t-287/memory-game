ad12f8855b34d7547a9c75789f5ef0aac150224b
Stephan <stephan287@gmail.com>
2020-10-14 14:49:59 -0400

    Updates

---
+++ precache-manifest.d7984900e6c0d0dc53c60da6bf5fe677.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a583970ab5e8a0e94f2585d27b0b6b5",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "5285bbf99e8ee3a9014f",
+    "url": "/memory-game/static/css/main.46b2bcd4.chunk.css"
+  },
+  {
+    "revision": "0ba400fcfe9457960990",
+    "url": "/memory-game/static/js/2.29c0361c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.29c0361c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5285bbf99e8ee3a9014f",
+    "url": "/memory-game/static/js/main.ba26a16c.chunk.js"
+  },
+  {
+    "revision": "61c60c7da2d87ca0facf",
+    "url": "/memory-game/static/js/runtime-main.32bcac6c.js"
+  },
+  {
+    "revision": "69aa3b9eb460c59e79d428ebd3cdbd53",
+    "url": "/memory-game/static/media/tenor.69aa3b9e.gif"
+  },
+  {
+    "revision": "055c150a59c71b138134e842c82990ac",
+    "url": "/memory-game/static/media/tenor10.055c150a.gif"
+  },
+  {
+    "revision": "683d2a251c12d5530e2bf17f7db7b770",
+    "url": "/memory-game/static/media/tenor11.683d2a25.gif"
+  },
+  {
+    "revision": "e71908f593066230e705b33dbb86c49c",
+    "url": "/memory-game/static/media/tenor12.e71908f5.gif"
+  },
+  {
+    "revision": "a6fdff7741f295326484c2f2d151e3b7",
+    "url": "/memory-game/static/media/tenor13.a6fdff77.gif"
+  },
+  {
+    "revision": "8f43b67372591ac50a33b803ab5a4168",
+    "url": "/memory-game/static/media/tenor2.8f43b673.gif"
+  },
+  {
+    "revision": "f630eda5b99763dcad6d16235a1349d2",
+    "url": "/memory-game/static/media/tenor3.f630eda5.gif"
+  },
+  {
+    "revision": "7a990dc1390ead4d2a488f146cb4d413",
+    "url": "/memory-game/static/media/tenor4.7a990dc1.gif"
+  },
+  {
+    "revision": "91e74af3e78b997ac81d399d84cfc923",
+    "url": "/memory-game/static/media/tenor5.91e74af3.gif"
+  },
+  {
+    "revision": "1ef73abaace1f85426f3d1b767a9934b",
+    "url": "/memory-game/static/media/tenor6.1ef73aba.gif"
+  },
+  {
+    "revision": "d10ce95b8bc06478f01f37e5468c0e06",
+    "url": "/memory-game/static/media/tenor7.d10ce95b.gif"
+  },
+  {
+    "revision": "36029a314fe92da70bdf49d0c9ec3705",
+    "url": "/memory-game/static/media/tenor8.36029a31.gif"
+  },
+  {
+    "revision": "452c2fce3953c8ac7d3006851b9b8ce3",
+    "url": "/memory-game/static/media/tenor9.452c2fce.gif"
+  }
+]);
